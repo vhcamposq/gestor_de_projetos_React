05ad7ec21ee843e984d700787b798468a269a2db
vhcamposq <viniciushcquadros@hotmail.com>
2022-03-30 17:13:27 -0300

    Criando estrutura

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <p>Costs</p>
+      <Router>
+        <div>
+          <Link to = '/'>Home</Link>
+          <Link to = '/company'>Empresa</Link>
+          <Link to = '/contact'>Contato</Link>
+          <Link to = '/newproject'>Novo Projeto</Link>
+        </div>
+        <Switch>
+          <Route exact path = '/'><Home/></Route>
+          <Route exact path = '/company'><Company/></Route>
+          <Route exact path = '/contact'><Contact/></Route>
+          <Route exact path = '/newproject'><NewProject/></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
