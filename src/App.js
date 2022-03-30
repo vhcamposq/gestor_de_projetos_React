081f98f9b2c8f53ca122510e1bf1674bbad15dd7
vhcamposq <viniciushcquadros@hotmail.com>
2022-03-30 17:36:43 -0300

    Estruturando Links do menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 
+import Container from './components/layout/Container'
 
 
 
@@ -18,11 +19,15 @@ function App() {
           <Link to = '/newproject'>Novo Projeto</Link>
         </div>
         <Switch>
-          <Route exact path = '/'><Home/></Route>
-          <Route exact path = '/company'><Company/></Route>
-          <Route exact path = '/contact'><Contact/></Route>
-          <Route exact path = '/newproject'><NewProject/></Route>
+          <Container customClass='min-height'>
+            <Route exact path = '/'><Home/></Route>
+            <Route exact path = '/company'><Company/></Route>
+            <Route exact path = '/contact'><Contact/></Route>
+            <Route exact path = '/newproject'><NewProject/></Route>
+            
+          </Container>
         </Switch>
+       <p>Footer</p>
       </Router>
     </div>
   );
